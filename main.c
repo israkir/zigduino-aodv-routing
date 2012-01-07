4d7c3404f9c6ed0e546a7eb7bcf21438ae703254
Hakki Kirmizi <israkir@hotmail.com>
2012-01-07 23:41:30 +0800

    source explanation on header is cleaned.

--- main.c
+++ main.c
@@ -1,26 +1,3 @@
-/******************************************************************************
- *  Nano-RK, a real-time operating system for sensor networks.
- *  Copyright (C) 2007, Real-Time and Multimedia Lab, Carnegie Mellon University
- *  All rights reserved.
- *
- *  This is the Open Source Version of Nano-RK included as part of a Dual
- *  Licensing Model. If you are unsure which license to use please refer to:
- *  http://www.nanork.org/nano-RK/wiki/Licensing
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, version 2.0 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *******************************************************************************/
-
 #include <nrk.h>
 #include <include.h>
 #include <ulib.h>
