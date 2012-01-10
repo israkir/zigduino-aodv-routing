06a27dcc7f41cc8bc60578465329d64fc3f96480
Penn Su <pennsuaccount@gmail.com>
2012-01-11 02:18:25 +0800

    change from printing ASCII number to character

--- main.c
+++ main.c
@@ -163,7 +163,7 @@ void rx_task ()
           if(aodvmsg.dest == node_addr){
             // this node is destination, so print received packet
             printf("[RX-DATA] ");
-            for(i=0; i<aodvmsg.msg_len; i++ ) printf("[%d]", rxmsg[i]);
+            for(i=0; i<aodvmsg.msg_len; i++ ) printf("[%c]", aodvmsg.msg[i]);
             printf("\r\n");
             memset(rxmsg, 0, aodvmsg.msg_len);
           } else {
